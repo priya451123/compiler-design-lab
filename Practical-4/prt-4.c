b00f0db9bed50f09e4d6cb10d8cc2d59b1dd4deb
Priya Karangula <154214021+priya451123@users.noreply.github.com>
2026-03-16 00:23:17 +0530

    Update prt-4.c

--- Practical-4/prt-4.c
+++ Practical-4/prt-4.c
@@ -1,1 +1,22 @@
+#include<stdio.h>
+#include<string.h>
 
+int main() {
+    char stack[20],input[20];
+    int top=-1,i=0;
+
+    printf("Enter string: ");
+    scanf("%s",input);
+
+    stack[++top]='$';
+    stack[++top]='E';
+
+    while(top>=0) {
+        printf("Stack top: %c\n",stack[top]);
+        top--;
+    }
+
+    printf("Parsing completed\n");
+
+    return 0;
+}
