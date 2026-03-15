15314a4a04d4c782ca9b022ed1262ee20c3abf84
Priya Karangula <154214021+priya451123@users.noreply.github.com>
2026-03-16 00:24:51 +0530

    Update prt-6.c

--- Practical-6/prt-6.c
+++ Practical-6/prt-6.c
@@ -1,1 +1,22 @@
 
+#include<stdio.h>
+
+int main() {
+    char stack[20],input[20];
+    int top=-1,i=0;
+
+    printf("Enter input string: ");
+    scanf("%s",input);
+
+    stack[++top]='$';
+
+    while(input[i]!='\0') {
+        stack[++top]=input[i];
+        printf("Shift %c\n",input[i]);
+        i++;
+    }
+
+    printf("Parsing Finished\n");
+
+    return 0;
+}
