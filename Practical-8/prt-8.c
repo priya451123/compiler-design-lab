c1b1844f1c815b57057ccde3ccd1375a36688d5a
Priya Karangula <154214021+priya451123@users.noreply.github.com>
2026-03-16 00:27:58 +0530

    Update prt-8.c

--- Practical-8/prt-8.c
+++ Practical-8/prt-8.c
@@ -1,1 +1,9 @@
 
+#include<stdio.h>
+
+int main() {
+    printf("LEX: Lexical analyzer generator\n");
+    printf("FLEX: Fast Lexical analyzer\n");
+    printf("Used to generate scanners for compilers\n");
+    return 0;
+}
