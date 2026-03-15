e36aa74356c8df41d39eb7af959b44c951e138c8
Priya Karangula <154214021+priya451123@users.noreply.github.com>
2026-03-16 00:24:08 +0530

    Update prt-5.c

--- Practical-5/prt-5.c
+++ Practical-5/prt-5.c
@@ -1,1 +1,39 @@
 
+#include<stdio.h>
+
+char str[20];
+int i=0;
+
+void E();
+void E1();
+
+void E() {
+    if(str[i]=='i') {
+        i++;
+        E1();
+    }
+}
+
+void E1() {
+    if(str[i]=='+') {
+        i++;
+        if(str[i]=='i') {
+            i++;
+            E1();
+        }
+    }
+}
+
+int main() {
+    printf("Enter string: ");
+    scanf("%s",str);
+
+    E();
+
+    if(str[i]=='\0')
+        printf("Valid string\n");
+    else
+        printf("Invalid string\n");
+
+    return 0;
+}
