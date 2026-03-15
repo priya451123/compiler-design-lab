5fd6666508d7362a5f9b4687c57181e576af0a5e
Priya Karangula <154214021+priya451123@users.noreply.github.com>
2026-03-16 00:12:50 +0530

    Update prt-1.c

--- Practical-1/prt-1.c
+++ Practical-1/prt-1.c
@@ -1,1 +1,23 @@
+#include<stdio.h>
+#include<ctype.h>
 
+int main() {
+    char ch;
+    FILE *fp;
+    int keywords=0, identifiers=0, numbers=0;
+
+    fp=fopen("input.txt","r");
+
+    while((ch=fgetc(fp))!=EOF) {
+        if(isalpha(ch))
+            identifiers++;
+        else if(isdigit(ch))
+            numbers++;
+    }
+
+    printf("Identifiers: %d\n",identifiers);
+    printf("Numbers: %d\n",numbers);
+
+    fclose(fp);
+    return 0;
+}
